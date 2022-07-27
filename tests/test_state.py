14fc812903451be1bbb533251846a34a9f2dfb3a
Bill Huang <bill.huang2001@gmail.com>
2022-07-26 17:32:52 -0700

    format code

--- tests/test_state.py
+++ tests/test_state.py
@@ -4,18 +4,16 @@ import jax
 import jax.numpy as jnp
 import pytest
 
+
 @exl.use_state_class
 class Leaf(exl.Module):
     def setup(self, key):
-        return {
-            'c': jnp.arange(10)
-        }
+        return {"c": jnp.arange(10)}
 
     def run(self, state):
-        c = state['c']
-        return {
-            'c': c * 2
-        }
+        c = state["c"]
+        return {"c": c * 2}
+
 
 @exl.use_state_class
 class Root(exl.Module):
@@ -25,35 +23,26 @@ class Root(exl.Module):
         self.leaf = Leaf()
 
     def setup(self, key):
-        return {
-            'attr_a': self.a,
-            'attr_b': self.b
-        }
+        return {"attr_a": self.a, "attr_b": self.b}
 
     def run(self, state):
-        attr_a = state['attr_a'] + 2
-        attr_b = state['attr_b'] - 2
+        attr_a = state["attr_a"] + 2
+        attr_b = state["attr_b"] - 2
         state = self.leaf.run(state)
-        return state | {'attr_a': attr_a, 'attr_b': attr_b}, 789
+        return state | {"attr_a": attr_a, "attr_b": attr_b}, 789
 
 
 def test_basic():
     key = jax.random.PRNGKey(123)
     test_module = Root()
     state = test_module.init(key=key)
-    chex.assert_trees_all_close(state, {
-        'attr_a': 123,
-        'attr_b': 456,
-        '_submodule_leaf': {
-            'c': jnp.arange(10)
-        }
-    })
+    chex.assert_trees_all_close(
+        state, {"attr_a": 123, "attr_b": 456, "_submodule_leaf": {"c": jnp.arange(10)}}
+    )
     state, magic = test_module.run(state)
-    chex.assert_trees_all_close(state, state, {
-        'attr_a': 125,
-        'attr_b': 454,
-        '_submodule_leaf': {
-            'c': jnp.arange(10) * 2
-        }
-    })
+    chex.assert_trees_all_close(
+        state,
+        state,
+        {"attr_a": 125, "attr_b": 454, "_submodule_leaf": {"c": jnp.arange(10) * 2}},
+    )
     assert magic == 789
