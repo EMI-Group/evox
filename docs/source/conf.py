e3a32f5d6c634e33b60bdde12baee9e0aa18bb6e
Bill Huang <bill.huang2001@gmail.com>
2022-07-28 21:43:30 -0700

    conf doc format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,6 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_typehints_format = 'short'
+autodoc_typehints = 'both'
