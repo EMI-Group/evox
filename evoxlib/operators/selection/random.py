102e59e064858e931565c2d4e4eae6b9a7a81fca
Bill Huang <bill.huang2001@gmail.com>
2022-07-30 13:16:11 -0700

    add random selection

---
+++ evoxlib/operators/selection/random.py
@@ -0,0 +1,19 @@
+import evoxlib as exl
+
+import jax
+import jax.numpy as jnp
+
+class RandomSelection(exl.Operator):
+    def __init__(self, p):
+        self.p = p
+
+    def setup(self, key):
+        return {
+            "key": key
+        }
+
+    def __call__(self, state, x):
+        key, subkey = jax.random.split(state["key"])
+        num = int(x.shape[0] * p)
+        chosen = jax.random.choice(subkey, x.shape[0], shape=(num, ))
+        return {"key": key}, x[chosen, :]
