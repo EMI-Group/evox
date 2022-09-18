e707f9cd4ea2c7d26b9666447816ee46f9be2110
ChristinaLee <1315552992@qq.com>
2022-09-18 20:26:48 +0800

    change the position of fitness argument

--- evoxlib/operators/selection/tournament.py
+++ evoxlib/operators/selection/tournament.py
@@ -6,18 +6,17 @@ import jax.numpy as jnp
 
 @exl.jit_class
 class TournamentSelection(exl.Operator):
-    def __init__(self, p, fitness):
+    def __init__(self, p):
         self.p = p
-        self.fitness = fitness
 
     def setup(self, key):
         return exl.State(key=key)
 
-    def __call__(self, state, x):
+    def __call__(self, state, x, fitness):
         key, subkey = jax.random.split(state.key)
         num = int(x.shape[0] * self.p)
         chosen = jax.random.choice(subkey, x.shape[0], shape=(num,))
         candidates = x[chosen, :]
-        best = max(candidates, key=lambda x: self.fitness(x))
+        best = max(candidates, key=lambda x: fitness(x))
         return exl.State(key=key), best
 
