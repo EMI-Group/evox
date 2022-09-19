b3dc36cba38c44b47302769e66e745860005c552
ChristinaLee <1315552992@qq.com>
2022-09-19 11:09:42 +0800

    change deterministic to stochastic

--- evoxlib/operators/selection/tournament.py
+++ evoxlib/operators/selection/tournament.py
@@ -6,17 +6,29 @@ import jax.numpy as jnp
 
 @exl.jit_class
 class TournamentSelection(exl.Operator):
-    def __init__(self, p):
+    def __init__(self, p, k):
         self.p = p
+        self.k = k
 
     def setup(self, key):
         return exl.State(key=key)
 
     def __call__(self, state, x, fitness):
         key, subkey = jax.random.split(state.key)
-        num = int(x.shape[0] * self.p)
-        chosen = jax.random.choice(subkey, x.shape[0], shape=(num,))
-        candidates = x[chosen, :]
-        best = max(candidates, key=lambda x: fitness(x))
-        return exl.State(key=key), best
+        # randomly select k individuals to form candidates
+        chosen = jax.random.choice(subkey, x.shape[0], shape=(self.k,))
+        candidates = x[chosen,:]
+        candidates_fitness = fitness[chosen]
+        # sort candidates
+        order = jnp.argsort(candidates_fitness)[::-1]
+        candidates = candidates[order]
 
+        possibilities = jnp.ones(shape=(self.k,)) * self.p
+        temp = 1
+        for i in range(self.k):
+            possibilities[i] *= temp
+            temp *= 1 - self.p
+
+        # select an individual from k candidates by possibilities
+        chosen = jax.random.choice(subkey, self.k, key=possibilities, shape=(1,))
+        return exl.State(key=key), candidates[chosen, :]
