618f5243388d6c84e4eebf2bee26d581efbc38a7
Bill Huang <bill.huang2001@gmail.com>
2022-09-19 22:48:04 +0800

    a more generic TournamentSelection

--- evoxlib/operators/selection/tournament.py
+++ evoxlib/operators/selection/tournament.py
@@ -1,33 +1,45 @@
-from turtle import pos
-import evoxlib as exl
+from typing import Callable
 
+import evoxlib as exl
 import jax
 import jax.numpy as jnp
 
 
 @exl.jit_class
 class TournamentSelection(exl.Operator):
-    def __init__(self, p, k):
-        self.p = p
-        self.k = k
+    """Tournament selection"""
+
+    def __init__(
+        self, num_round: int, tournament_func: Callable = jnp.argmax, tournament_size: int = 2
+    ):
+        """
+        Parameters
+        ----------
+        num_round
+            Number of time the tournament will hold.
+        tournament_func
+            A function used to determine the winner of the tournament.
+            The function must accept a array of shape (N, ) or (N, K),
+            where N is the number of individuals and K is the number of objectives,
+            and return a single integer indicating the index of the winner.
+        tournament_size
+            Number of individuals in one tournament
+        """
+        self.num_round = num_round
+        self.tournament_func = tournament_func
+        self.tournament_size = tournament_size
 
     def setup(self, key):
         return exl.State(key=key)
 
     def __call__(self, state, x, fitness):
         key, subkey = jax.random.split(state.key)
-        # randomly select k individuals to form candidates
-        chosen = jax.random.choice(subkey, x.shape[0], shape=(self.k,))
-        candidates = x[chosen,:]
-        candidates_fitness = fitness[chosen]
-        # sort candidates
-        order = jnp.argsort(candidates_fitness)[::-1]
-        candidates = candidates[order]
-
-        possibilities = jnp.full((self.k, ), 1 - self.p)
-        possibilities[0] = 1
-        possibilities = jnp.cumprod(possibilities) * self.p
-
-        # select an individual from k candidates by possibilities
-        chosen = jax.random.choice(subkey, self.k, key=possibilities, shape=(1,))
-        return exl.State(key=key), candidates[chosen, :]
+        # select num_round times and each time
+        # k individuals to form candidates
+        chosen = jax.random.choice(
+            subkey, x.shape[0], shape=(self.num_round, self.tournament_size)
+        )
+        candidates = x[chosen, ...]
+        candidates_fitness = fitness[chosen, ...]
+        winner_indices = jax.vmap(self.tournament_func)(candidates_fitness)
+        return exl.State(key=key), candidates[winner_indices, ...]
