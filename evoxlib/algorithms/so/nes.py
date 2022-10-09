8fa0123d4cfdd29d3a8938a86a5679ef9bcf0d6a
sses7757 <skk7757@163.com>
2022-10-09 12:10:25 +0800

    implement SNES

--- evoxlib/algorithms/so/nes.py
+++ evoxlib/algorithms/so/nes.py
@@ -76,3 +76,66 @@ class xNES(exl.Algorithm):
 
         population, zero_mean_pop, key = self.new_pop(key, mean, sigma, B)
         return state.update(population=population, zero_mean_pop=zero_mean_pop, mean=mean, sigma=sigma, B=B, key=key)
+
+
+@exl.jit_class
+class SeparableNES(exl.Algorithm):
+    def __init__(self, init_mean, init_var, pop_size=None, recombination_weights=None, learning_rate_mean=None, learning_rate_var=None):
+        """
+        See [link](https://arxiv.org/pdf/1106.4487.pdf) for default parameters
+        """
+        assert(pop_size > 0)
+        assert(learning_rate_mean > 0 and learning_rate_var > 0)
+        self.dim = init_mean.shape[0]
+        self.init_mean = init_mean
+        assert(init_var.shape == (self.dim,))
+        self.init_var = init_var
+        if pop_size is None:
+            pop_size = 4 + math.floor(3 * math.log(self.dim))
+        self.pop_size = pop_size
+        if learning_rate_mean is None:
+            learning_rate_mean = 1
+        self.learning_rate_mean = learning_rate_mean
+        if learning_rate_var is None:
+            learning_rate_var = (3 + math.log(self.dim)) / 5 / math.sqrt(self.dim)
+        self.learning_rate_var = learning_rate_var
+        if recombination_weights is None:
+            recombination_weights = math.log(pop_size / 2 + 1) - jnp.log(jnp.arange(1, pop_size + 1))
+            recombination_weights = jnp.clip(recombination_weights, 0)
+            recombination_weights = recombination_weights / jnp.sum(recombination_weights) - 1 / pop_size
+        else:
+            assert(recombination_weights.shape == (self.dim,))
+        self.weight = recombination_weights
+
+    def new_pop(self, key, mean, sigma):
+        key, sample_key = jax.random.split(key)
+        zero_mean_pop = jax.random.normal(sample_key, shape=(self.pop_size, self.dim))
+        population = lax.map(lambda p: mean + sigma * p, zero_mean_pop)
+        return population, zero_mean_pop, key
+
+    def setup(self, key):
+        mean = self.init_mean
+        sigma = self.init_var
+        
+        population, zero_mean_pop, key = self.new_pop(key, mean, sigma)
+        return exl.State(population=population, zero_mean_pop=zero_mean_pop, mean=mean, sigma=sigma, key=key)
+
+    def ask(self, state):
+        return state, state.population
+
+    def tell(self, state, fitness):
+        mean = state.mean
+        sigma = state.sigma
+        population = state.population
+        zero_mean_pop = state.zero_mean_pop
+        
+        fitness, population, zero_mean_pop = lax.sort((fitness, population, zero_mean_pop), stable=False)
+
+        grad_μ = jnp.sum(lax.map(lambda u, s: u * s, zip(self.weight, zero_mean_pop)), axis=1)
+        grad_σ = jnp.sum(lax.map(lambda u, s: u * (s * s - 1), zip(self.weight, zero_mean_pop)), axis=1)
+
+        mean += self.learning_rate_mean * sigma * grad_μ
+        sigma *= jnp.exp(self.learning_rate_var / 2 * grad_σ)
+
+        population, zero_mean_pop, key = self.new_pop(key, mean, sigma)
+        return state.update(population=population, zero_mean_pop=zero_mean_pop, mean=mean, sigma=sigma, key=key)
