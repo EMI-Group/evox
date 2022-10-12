7bfccbc4f3322a8e37366bc7bbc2b714ffb12c4e
Bill Huang <bill.huang2001@gmail.com>
2022-10-12 22:43:29 +0800

    init examples

--- examples/pso_ackley.py
+++ examples/pso_ackley.py
@@ -5,14 +5,12 @@ import jax.numpy as jnp
 
 # create a pipeline
 pipeline = pipelines.StdPipeline(
-    algorithms.CSO(
+    algorithms.PSO(
         lb=jnp.full(shape=(2,), fill_value=-32),
         ub=jnp.full(shape=(2,), fill_value=32),
         pop_size=100,
     ),
     problems.classic.Ackley(),
-    fitness_monitor=True,
-    population_monitor=True
 )
 
 # init the pipeline
@@ -22,6 +20,3 @@ state = pipeline.init(key)
 # run the pipeline for 100 steps
 for i in range(100):
     state = pipeline.step(state)
-
-pipeline.fitness_monitor.show()
-pipeline.population_monitor.show()
