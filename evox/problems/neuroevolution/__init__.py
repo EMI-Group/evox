e40bc7d312bedcf56c95066ded7e8104e24fbcb6
Bill Huang <bill.huang2001@gmail.com>
2022-11-21 21:12:33 +0800

    handle optional dependencies

--- evox/problems/neuroevolution/__init__.py
+++ evox/problems/neuroevolution/__init__.py
@@ -1,2 +1,23 @@
 from .torchvision_dataset import TorchvisionDataset
-from .gym import Gym
+
+try:
+    # optional dependency: torchvision, optax
+    from .torchvision_dataset import TorchvisionDataset
+except ImportError as e:
+    original_erorr_msg = str(e)
+
+    def TorchvisionDataset(*args, **kwargs):
+        raise ImportError(
+            f'TorchvisionDataset requires torchvision, optax but got "{original_erorr_msg}" when importing'
+        )
+
+try:
+    # optional dependency: gym
+    from .gym import Gym
+except ImportError as e:
+    original_erorr_msg = str(e)
+
+    def Gym(*args, **kwargs):
+        raise ImportError(
+            f'Gym requires gym, ray but got "{original_erorr_msg}" when importing'
+        )
