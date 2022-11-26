5c608836e1f2f1bdd2e93a7a0a620781ddad07f9
Bill Huang <bill.huang2001@gmail.com>
2022-11-26 15:23:07 +0800

    update benchmark

--- examples/distributed_benchmark.py
+++ examples/distributed_benchmark.py
@@ -1,7 +1,6 @@
 import time
 
 import evox
-import evox as ex
 import jax
 import jax.numpy as jnp
 import pytest
@@ -15,33 +14,35 @@ class SimpleCNN(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(features=6, kernel_size=(3, 3), padding="SAME")(x)
-        x = nn.relu(x)
-        x = nn.Conv(features=16, kernel_size=(3, 3), padding="SAME")(x)
+        # normalize
+        x = x / 255.0
+        x = (x - 0.5) / 0.5
+
+        x = nn.Conv(features=16, kernel_size=(3, 3), padding="VALID")(x)
         x = nn.relu(x)
         x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = nn.Conv(features=16, kernel_size=(3, 3), padding="SAME")(x)
-        x = nn.relu(x)
-        x = nn.Conv(features=16, kernel_size=(3, 3), padding="SAME")(x)
+
+        x = nn.Conv(features=16, kernel_size=(3, 3), padding="VALID")(x)
         x = nn.relu(x)
         x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
 
         x = x.reshape(x.shape[0], -1)
-        x = nn.Dense(120)(x)
-        x = nn.sigmoid(x)
+        x = nn.Dense(64)(x)
+        x = nn.relu(x)
         x = nn.Dense(10)(x)
         return x
 
 
 def init_problem_and_model(key):
     model = SimpleCNN()
-    batch_size = 128
-    initial_params = model.init(key, jnp.zeros((batch_size, 32, 32, 3)))
-    problem = ex.problems.neuroevolution.TorchvisionDataset(
-        root="./datasets",
+    batch_size = 256
+    initial_params = jax.jit(model.init)(key, jnp.zeros((batch_size, 32, 32, 3)))
+    problem = evox.problems.neuroevolution.TorchvisionDataset(
+        root="/cifar10",  # the path to cifar10 dataset
         batch_size=batch_size,
-        forward_func=model.apply,
+        forward_func=jax.jit(model.apply),
         dataset_name="cifar10",
+        in_memory=True,
     )
     return initial_params, problem
 
@@ -51,35 +52,55 @@ def run_benchmark(num_gpus):
     pipeline_key, model_init_key = jax.random.split(key)
     initial_params, problem = init_problem_and_model(model_init_key)
 
-    start = time.perf_counter()
-    adapter = ex.utils.TreeAndVector(initial_params)
+    adapter = evox.utils.TreeAndVector(initial_params)
     monitor = FitnessMonitor()
+
     algorithm = algorithms.PGPE(
-        100,
+        150,
         adapter.to_vector(initial_params),
-        "adam",
+        "clipup",
         center_learning_rate=0.01,
         stdev_init=0.01,
     )
-    pipeline = pipelines.StdPipeline(
+    pipeline = pipelines.DistributedPipeline(
         algorithm=algorithm,
         problem=problem,
+        pop_size=150,
+        num_workers=num_gpus,
+        options={"num_gpus": 1, "num_cpus": 8},
         pop_transform=adapter.batched_to_tree,
-        fitness_transform=monitor.update,
+        global_fitness_transform=monitor.update
     )
     # init the pipeline
     state = pipeline.init(key)
+    # warm up
+    for i in range(3):
+        pipeline.step(state)
+        # _state, accuracy = pipeline.valid(state, metric="accuracy")
 
-    # run the pipeline for 100 steps
-    for i in range(3000):
+    start = time.perf_counter()
+    valid_acc = []
+    # run the pipeline for 1000 steps
+    for i in range(5000):
         state = pipeline.step(state)
+        # if (i + 1) % 100 == 0:
+        #     state, accuracy = pipeline.valid(state, metric="accuracy")
+        #     valid_acc.append(accuracy)
 
-    # the result should be close to 0
-    min_fitness = monitor.get_min_fitness()
-    print(f"Adapter loss: {min_fitness}  time: {time.perf_counter() - start}")
+    return time.perf_counter() - start, valid_acc, monitor.history
 
 
-MAX_GPUS = 1
-
+MAX_GPUS = 6
+result = {}
 for i in range(1, MAX_GPUS + 1):
-    run_benchmark(i)
+    print(f"using {i} gpus")
+    runtime, valid_acc, train_loss = run_benchmark(i)
+    print([jnp.max(x) for x in valid_acc])
+    result[i] = {
+        "runtime": runtime,
+        "valid_acc": valid_acc,
+        "train_loss": train_loss
+    }
+
+with open("exp/distributed_result.json", "w") as f:
+    json.dump(result, f)
--- examples/scale_benchmark.py
+++ examples/scale_benchmark.py
@@ -6,8 +6,19 @@ import time
 import json
 
 
+backend = jax.default_backend()
 num_rounds = 1
+dim_lists = jnp.arange(0, 4 + 1/4, 1/4)
+dim_lists = jnp.round(10 ** dim_lists).astype(jnp.int32)
+dim_lists = dim_lists.tolist()
 
+pop_lists = jnp.arange(0, 5 + 1/4, 1/4)
+pop_lists = jnp.round(10 ** pop_lists).astype(jnp.int32)
+pop_lists = pop_lists.tolist()
+
+print(dim_lists)
+print(pop_lists)
+print(f"backend: {backend}")
 
 def run_benchmark(dim, pop_size):
     init_mean = jnp.zeros((dim,))
@@ -18,26 +29,28 @@ def run_benchmark(dim, pop_size):
     state = pipeline.init(key)
 
     # warm up
-    for i in range(10):
+    for i in range(3):
         pipeline.step(state)
     # real benchmark
     start = time.perf_counter()
-    for i in range(1000):
+    for i in range(100):
         state = pipeline.step(state)
     end = time.perf_counter()
     return end - start
 
 
 dim_scale_result = {}
-for dim in [10, 100, 1000, 10_000]:
+for dim in dim_lists:
+    print(dim)
     dim_scale_result[dim] = [run_benchmark(dim, 100) for i in range(num_rounds)]
 
-with open("log/dim_scale_result.json", "w") as f:
+with open(f"exp/{backend}_dim_scale_result.json", "w") as f:
     json.dump(dim_scale_result, f)
 
 pop_scale_result = {}
-for pop_size in [10, 100, 1000, 10_000, 100_000]:
+for pop_size in pop_lists:
+    print(pop_size)
     pop_scale_result[pop_size] = [run_benchmark(100, pop_size) for i in range(num_rounds)]
 
-with open("log/pop_scale_result.json", "w") as f:
+with open(f"exp/{backend}_pop_scale_result.json", "w") as f:
     json.dump(pop_scale_result, f)
