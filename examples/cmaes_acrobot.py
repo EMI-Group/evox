6241cd9213c650bb7216d71b74b4727ef75dada4
ChristinaLee <1315552992@qq.com>
2023-01-06 22:25:21 +0800

    add acrobot examples

---
+++ examples/cmaes_acrobot.py
@@ -0,0 +1,67 @@
+from evox import pipelines, algorithms, problems
+from evox.monitors import FitnessMonitor
+from evox.utils import TreeAndVector
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+
+class AcrobotPolicy(nn.Module):
+    """A simple model for Acrobot"""
+
+    @nn.compact
+    def __call__(self, x):
+        x = x.at[1].multiply(10)  # normalization
+        x = nn.Dense(16)(x)
+        x = nn.relu(x)
+        x = nn.Dense(3)(x)
+
+        return jnp.argmax(x)
+
+
+key = jax.random.PRNGKey(42)
+model_key, pipeline_key = jax.random.split(key)
+
+model = AcrobotPolicy()
+params = model.init(model_key, jnp.zeros((6,)))
+adapter = TreeAndVector(params)
+monitor = FitnessMonitor()
+problem = problems.neuroevolution.Gym(
+    env_name="Acrobot-v1",
+    policy=jax.jit(model.apply),
+    num_workers=16,
+    env_per_worker=4,
+    controller_options={
+        "num_cpus": 0,
+        "num_gpus": 0,
+    },
+    worker_options={"num_cpus": 1, "num_gpus": 1 / 16},
+    batch_policy=False,
+)
+center = adapter.to_vector(params)
+# create a pipeline
+pipeline = pipelines.StdPipeline(
+    algorithm=algorithms.CMAES(
+        init_mean=center,
+        init_stdev=1,
+        pop_size=64
+    ),
+    problem=problem,
+    pop_transform=adapter.batched_to_tree,
+    fitness_transform=monitor.update,
+)
+# init the pipeline
+state = pipeline.init(pipeline_key)
+# run the pipeline for 100 steps
+for i in range(100):
+    print(monitor.get_min_fitness())
+    state = pipeline.step(state)
+
+state, sample_pop = pipeline.sample(state)
+# problem._render(state.get_child_state("problem"), adapter.to_tree(sample_pop[0]))
+
+min_fitness = monitor.get_min_fitness()
+print(min_fitness)
+print(monitor.history)
+
+# 61.0
+# [102.0, 101.0, 101.0, 101.0, 97.0, 78.0, 78.0, 65.0, 65.0, 62.0, 62.0, 62.0, 62.0, 62.0, 62.0, 62.0, 62.0, 62.0, 62.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0]
