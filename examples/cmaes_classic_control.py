04c6dbf9118dff9d5058f0e8cb7f635967cedcb1
ChristinaLee <1315552992@qq.com>
2023-01-07 11:46:37 +0800

    combine 5 examples into one file

--- examples/cmaes_classic_control.py
+++ examples/cmaes_classic_control.py
@@ -5,28 +5,41 @@ import jax
 import jax.numpy as jnp
 from flax import linen as nn
 
-class AcrobotPolicy(nn.Module):
-    """A simple model for Acrobot"""
+# change here only
+gym_name = "Pendulum-v1"
+
+def tanh2(x):
+    return 2*nn.tanh(x)
+
+classic_params = {
+    "Acrobot-v1":(3, (6,), jnp.argmax), 
+    "CartPole-v1":(2, (4,), jnp.argmax),
+    "MountainCarContinuous-v0":(1, (2,), nn.tanh),
+    "MountainCar-v0":(3, (2,), jnp.argmax),
+    "Pendulum-v1":(1, (3,), tanh2)
+}
+
+class ClassicPolicy(nn.Module):
+    """A simple model for Classic Control problem"""
 
     @nn.compact
     def __call__(self, x):
         x = x.at[1].multiply(10)  # normalization
         x = nn.Dense(16)(x)
         x = nn.relu(x)
-        x = nn.Dense(3)(x)
+        x = nn.Dense(classic_params[gym_name][0])(x)
 
-        return jnp.argmax(x)
-
+        return classic_params[gym_name][2](x)
 
 key = jax.random.PRNGKey(42)
 model_key, pipeline_key = jax.random.split(key)
 
-model = AcrobotPolicy()
-params = model.init(model_key, jnp.zeros((6,)))
+model = ClassicPolicy()
+params = model.init(model_key, jnp.zeros(classic_params[gym_name][1]))
 adapter = TreeAndVector(params)
 monitor = FitnessMonitor()
 problem = problems.neuroevolution.Gym(
-    env_name="Acrobot-v1",
+    env_name=gym_name,
     policy=jax.jit(model.apply),
     num_workers=16,
     env_per_worker=4,
@@ -40,11 +53,10 @@ problem = problems.neuroevolution.Gym(
 center = adapter.to_vector(params)
 # create a pipeline
 pipeline = pipelines.StdPipeline(
-    algorithm=algorithms.PGPE(
-        optimizer="adam",
-        center_init=center,
-        pop_size=64,
-        stdev_init=1
+    algorithm=algorithms.CMAES(
+        init_mean=center,
+        init_stdev=1,
+        pop_size=64
     ),
     problem=problem,
     pop_transform=adapter.batched_to_tree,
@@ -63,6 +75,3 @@ state, sample_pop = pipeline.sample(state)
 min_fitness = monitor.get_min_fitness()
 print(min_fitness)
 print(monitor.history)
-
-# 60.0
-# [124.0, 124.0, 121.0, 121.0, 84.0, 84.0, 84.0, 84.0, 84.0, 64.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 61.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0]
