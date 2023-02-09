8244e38a0edf6451ec7eadc6cb0fe357eda24c6a
Bill Huang <bill.huang2001@gmail.com>
2023-02-09 12:17:53 +0800

    add github link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,10 @@ autodoc_mock_imports = [
 #
 html_theme = 'pydata_sphinx_theme'
 
+html_theme_options = {
+  "github_url": "https://github.com/EMI-Group/evox",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
