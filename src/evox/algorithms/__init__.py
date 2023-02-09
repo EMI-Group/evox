e046b38e8df2b3235acbdc3056ca4b7961c99f20
Bill Huang <bill.huang2001@gmail.com>
2023-02-09 20:38:52 +0800

    add docstring

--- src/evox/algorithms/__init__.py
+++ src/evox/algorithms/__init__.py
@@ -1,3 +1,7 @@
+"""The module that contains all pre-defined algorithms,
+including both single-objective algorithms and multi-objective algorithms.
+"""
+
 from .mo import *
 from .so import *
 from .containers import *
--- src/evox/problems/__init__.py
+++ src/evox/problems/__init__.py
@@ -1,1 +1,3 @@
+"""The module that contains all pre-defined problems"""
+
 from . import classic, neuroevolution, rl
