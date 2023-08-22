00b887157f8bdcfb0d729badb37b3be4351335f3
wls2002 <cvgcvg@163.com>
2023-08-22 16:50:12 +0800

    solve conflict

--- src/evox/algorithms/so/__init__.py
+++ src/evox/algorithms/so/__init__.py
@@ -1,6 +1,10 @@
 from .cso import CSO
 from .pso import PSO
 from .de import DE
+from .code import CoDE
+from .jade import JaDE
+from .sade import SaDE
+from .shade import SHADE
 
 from .pso_vatients import *
 from .es import *
