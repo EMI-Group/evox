6a99314c964eeb49554ef67a9f03c050846d54a8
Zhenyu <1370096263@qq.com>
2023-08-22 15:52:18 +0800

    update __init__.py

--- src/evox/operators/selection/__init__.py
+++ src/evox/operators/selection/__init__.py
@@ -1,6 +1,5 @@
 from .find_pbest import select_rand_pbest
 from .roulette_wheel import RouletteWheelSelection
-from .knea_selection import KnEASelection
 from .rvea_selection import ReferenceVectorGuided, ref_vec_guided
 from .topk_fit import TopkFit, topk_fit
 from .tournament import Tournament, tournament_multi_fit, tournament_single_fit
