258ea5e2f25dffb3d370332bfa8ea5886b920a4c
Bill Huang <bill.huang2001@gmail.com>
2023-10-08 22:29:05 +0800

    dev: add functional primitives

--- src/evox/problems/numerical/rosenbrock.py
+++ src/evox/problems/numerical/rosenbrock.py
@@ -8,9 +8,11 @@ def _rosenbrock_func(x):
         100 * ((x[1:])- x[:x.shape[0] - 1] ** 2) ** 2 + (x[:x.shape[0] - 1] - 1) ** 2)
     return f
 
+def rosenbrock_func(X):
+    return jax.vmap(_rosenbrock_func)(X)
+
 
 @jit_class
 class Rosenbrock(Problem):
     def evaluate(self, state, X):
-        return jax.vmap(_rosenbrock_func)(X), state
-
+        return rosenbrock_func(X), state
--- src/evox/problems/numerical/sphere.py
+++ src/evox/problems/numerical/sphere.py
@@ -2,6 +2,10 @@ import jax.numpy as jnp
 from evox import Problem, jit_class
 
 
+def sphere_func(X):
+    return jnp.sum(X**2, axis=-1)
+
+
 @jit_class
 class Sphere(Problem):
     def __init__(self, a=20, b=0.2, c=2):
@@ -10,5 +14,4 @@ class Sphere(Problem):
         self.c = c
 
     def evaluate(self, state, X):
-        F = jnp.sum(X**2, axis=-1)
-        return F, state
+        return sphere_func(X), state
