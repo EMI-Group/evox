961ec3988f3f33c25f6d1c909a44d1cf8749d5c1
jasciiz <48852892+jasciiz@users.noreply.github.com>
2023-10-09 15:32:31 +0800

    add LSMOPs and MaFs Problems (#75)
    * lsmop/mafs
    
    * update
    
    * maf11
    
    * complete
    
    * maf check
    
    * finish maf5 check
    
    * finish maf9 check
    
    * finish maf10 check
    
    * finish maf11 check
    
    * fori_loop
    
    * fori_loop
    
    * add ax.disable_jit()
    
    * maf10 JIT
    
    * maf13 JIT finish
    
    * JIT finish
    
    * maf LSMOP finish
    
    * Modify format
    
    * Modified to meet merger requirements. But still need change lsmop.py because of for loop, remove chex for type hint and redundant function liking Sphere.
    
    * Modified to meet merger requirements of "for" in lsmop.py. Still need to remove chex for type hint and redundant function liking Sphere.
    
    * remove chex for type hint
    
    * Remove redundant function in lsmop.py, remove "global" in two test files, fix the bug in griewank.py
    
    * Modify “import” format
    
    * Modify key format
    
    * Remove unnecessary comment
    
    * Change double for-loop nesting to one layer for-loop embedding fori_ Loop
    
    * Reduce redundant code, use double for-loops and fix other problems
    
    * Reduce redundant code
    
    * fix: follow numpy's style
    
    * dev: simplify code
    
    * dev: refactor code
    
    * fix a liitle problem.
    
    * fix a liitle problem, add more test problems for lsmop
    
    * Remove unnecessary comments
    
    ---------
    
    Co-authored-by: lh_cs <lh_cse@163.com>
    Co-authored-by: Bill Huang <bill.huang2001@gmail.com>

--- src/evox/problems/numerical/griewank.py
+++ src/evox/problems/numerical/griewank.py
@@ -4,7 +4,11 @@ from evox import Problem, jit_class
 
 
 def _griewank_func(x):
-    f = 1 / 4000 * jnp.mean(x) - jnp.prod(jnp.cos(x / jnp.sqrt(jnp.arange(1, x.shape[0] + 1)) )) + 1
+    f = (
+        1 / 4000 * jnp.sum(x**2)
+        - jnp.prod(jnp.cos(x / jnp.sqrt(jnp.arange(1, x.shape[0] + 1))))
+        + 1
+    )
     return f
 
 
@@ -14,6 +18,5 @@ def griewank_func(X):
 
 @jit_class
 class Griewank(Problem):
-
     def evaluate(self, state, X):
         return griewank_func(X), state
