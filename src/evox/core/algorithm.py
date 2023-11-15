f8d1e0991f5900a66fc0f80fb3dcf639bb08beec
Bill Huang <bill.huang2001@gmail.com>
2023-11-15 15:29:41 +0800

    doc: add doc for init_ask and init_tell

--- src/evox/core/algorithm.py
+++ src/evox/core/algorithm.py
@@ -11,9 +11,42 @@ class Algorithm(Stateful):
     """Base class for all algorithms"""
 
     def init_ask(self, state: State) -> Tuple[jax.Array, State]:
+        """Ask the algorithm for the initial population
+
+        Override this method if you need to initialize the population in a special way.
+        For example, Genetic Algorithm needs to evaluate the fitness of the initial population of size N,
+        but after that, it only need to evaluate the fitness of the offspring of size M, and N != M.
+        Since JAX requires the function return to have static shape, we need to have two different functions,
+        one is the normal `ask` and another is `init_ask`.
+
+        Parameters
+        ----------
+        state
+            The state of this algorithm.
+
+        Returns
+        -------
+        population
+            The candidate solution.
+        state
+            The new state of the algorithm.
+        """
         return None, State()
 
     def init_tell(self, state: State) -> State:
+        """Tell the algorithm the fitness of the initial population
+        Use in pair with `init_ask`.
+
+        Parameters
+        ----------
+        state
+            The state of this algorithm
+
+        Returns
+        -------
+        state
+            The new state of the algorithm
+        """
         return State()
 
     def ask(self, state: State) -> Tuple[jax.Array, State]:
