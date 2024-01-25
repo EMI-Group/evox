8bef09b9c8a71ccad94591c922675303c6c78481
Xinyao Li <xinyao.billy.li@gmail.com>
2024-01-25 22:47:52 +0800

    add detailed comments

--- src/evox/operators/gaussian_process/classification.py
+++ src/evox/operators/gaussian_process/classification.py
@@ -1,26 +1,41 @@
+# https://docs.jaxgaussianprocesses.com/examples/classification/
+
 from jax import config
 
 config.update("jax_enable_x64", True)
 
 import jax
-import jax.numpy as jnp
 import jax.random as jr
 
 import optax as ox
-import tensorflow_probability.substrates.jax as tfp
-
 import gpjax as gpx
 
 from gpjax.mean_functions import Zero
 from evox.operators.gaussian_process.kernels import RBF
 from gpjax.objectives import LogPosteriorDensity
 
-tfd = tfp.distributions
-identity_matrix = jnp.eye
 key = jr.PRNGKey(123)
 
 
 class GPClassification:
+    """
+    Gaussian Process Classification model using JAX, Optax and GPJAX.
+
+    This model applies Gaussian Process (GP) for classification tasks. It uses an RBF kernel and
+    a Zero mean function by default. The class allows for fitting the model to data and making predictions.
+    The GP's prior and posterior are defined, and the model is fitted using the specified optimization method.
+
+    Attributes:
+        kernel: The kernel function for the GP (default is RBF).
+                The difference between the kernels are shown in the link: https://docs.jaxgaussianprocesses.com/examples/intro_to_kernels/.
+        mean_fun: The mean function for the GP (default is Zero).
+        key: JAX random key for stochastic operations.
+        prior: The prior distribution of the GP, which is chosen by the user according to the specific problems.
+        likelihood: The likelihood function for the GP.
+        object: The objective function (default is negative Log Posterior Density).
+        posterior: The posterior distribution of the GP after applying the likelihood.
+    """
+
     def __init__(
         self,
         kernel=RBF(),
@@ -29,6 +44,7 @@ class GPClassification:
         object=LogPosteriorDensity(negative=True),
         key=jr.PRNGKey(123),
     ):
+        """Initializes the Gaussian Process Classification model with default parameters."""
         self.kernel = kernel
         self.mean_fun = meanfun
         self.key = key
@@ -37,19 +53,43 @@ class GPClassification:
         self.object = object
         self.posterior = self.prior * self.likelihood
 
-    def fit(self, x, y, optimzer=ox.adam):
+    def fit(self, x, y, optimizer=ox.adam):
+        """
+        Fits the model to the provided data.
+
+        The method takes in features (x) and labels (y), creates a dataset, and then uses
+        the specified optimization method to fit the model. The process includes setting
+        the objective function and optimizing the posterior distribution.
+
+        Args:
+            x: The feature matrix.
+            y: The label vector.
+            optimizer: The optimization algorithm implemented by Optax to use (default is Adam).
+        """
         self.dataset = gpx.Dataset(X=x, y=y)
         self.object(self.posterior, train_data=self.dataset)
         self.opt_posterior, self.history = gpx.fit(
             model=self.posterior,
             objective=self.object,
             train_data=self.dataset,
-            optim=optimzer,
+            optim=optimizer,
             num_iters=500,
             key=self.key,
         )
 
     def predict(self, x):
+        """
+        Makes predictions on new data.
+
+        The method computes the predictive distribution of the optimized model for new input features.
+        It returns the predictive distribution along with the mean and standard deviation of the predictions.
+
+        Args:
+            x: The feature matrix for which predictions are to be made.
+
+        Returns:
+            A tuple containing the predictive distribution, predictive mean, and predictive standard deviation.
+        """
         latent_dist = self.opt_posterior.predict(x, train_data=self.dataset)
         predictive_dist = self.opt_posterior.likelihood(latent_dist)
 
--- src/evox/operators/gaussian_process/regression.py
+++ src/evox/operators/gaussian_process/regression.py
@@ -1,24 +1,50 @@
 # https://docs.jaxgaussianprocesses.com/examples/regression/
 
 from jax import config
-from jax import jit
-import jax.numpy as jnp
-import jax.random as jr
-import gpjax as gpx
-# from gpjax.kernels import RBF
-from gpjax.mean_functions import Zero
-# from gpjax.likelihoods import Gaussian
-from gpjax.objectives import ConjugateMLL
-import jax
-import optax as ox
-from evox.operators.gaussian_process.kernels import RBF
-from evox.operators.gaussian_process.likelihoods import Gaussian
-# the demand of gpJAX. Enable Float64 for more stable matrix inversions.
+
 config.update("jax_enable_x64", True)
+
+import gpjax as gpx
+import jax
+from jax import jit
+import optax as ox
+import jax.random as jr
+
+from gpjax.objectives import ConjugateMLL
+from gpjax.mean_functions import Zero
+from evox.operators.gaussian_process.kernels import RBF
+
+
+
+
 class GPRegression:
+    """
+    Gaussian Process Regreesion model using JAX, Optax and GPJAX.
 
+    This model applies Gaussian Process (GP) for regression tasks. It uses an RBF kernel and
+    a Zero mean function by default. The class allows for fitting the model to data and making predictions.
+    The GP's prior and posterior are defined, and the model is fitted using the specified optimization method.
 
-    def __init__(self, kernel=RBF(), meanfun=Zero(), likelihood=None, object=ConjugateMLL(negative=True),key=jr.PRNGKey(123)):
+    Attributes:
+        kernel: The kernel function for the GP (default is RBF).
+                The difference between the kernels are shown in the link: https://docs.jaxgaussianprocesses.com/examples/intro_to_kernels/.
+        mean_fun: The mean function for the GP (default is Zero).
+        key: JAX random key for stochastic operations.
+        prior: The prior distribution of the GP, which is chosen by the user according to the specific problems.
+        likelihood: The likelihood function for the GP.
+        object: The objective function (default is Conjugate Marginal Log Likelihood).
+        posterior: The posterior distribution of the GP after applying the likelihood.
+    """
+
+    def __init__(
+        self,
+        kernel=RBF(),
+        meanfun=Zero(),
+        likelihood=None,
+        object=ConjugateMLL(negative=True),
+        key=jr.PRNGKey(123),
+    ):
+        """Initializes the Gaussian Process Regression model with default parameters."""
         self.kernel = kernel
         self.mean_fun = meanfun
         self.key = key
@@ -27,34 +53,55 @@ class GPRegression:
         self.object = object
         self.posterior = self.prior * self.likelihood
 
-
     def fit(self, x, y, optimzer=ox.GradientTransformation):
         self.dataset = gpx.Dataset(X=x, y=y)
         self.object(self.posterior, train_data=self.dataset)
-        # add jit
+        """
+        Fits the model to the provided data.
+
+        The method takes in features (x) and labels (y), creates a dataset, and then uses
+        the specified optimization method to fit the model. The process includes setting
+        the objective function and optimizing the posterior distribution.
+
+        Args:
+            x: The feature matrix.
+            y: The label vector.
+            optimizer: The optimization algorithm implemented by Optax to use (default is Gradient Transformation).
+        """
         self.opt_posterior, self.history = gpx.fit(
-            model= self.posterior,
-            objective= self.object,
-            train_data= self.dataset,
-            optim = optimzer,
+            model=self.posterior,
+            objective=self.object,
+            train_data=self.dataset,
+            optim=optimzer,
             num_iters=500,
-            key=self.key
+            key=self.key,
         )
 
-    def fit_scipy(self,x,y):
+    def fit_scipy(self, x, y):
         self.dataset = gpx.Dataset(X=x, y=y)
         self.object(self.posterior, train_data=self.dataset)
         self.opt_posterior, self.history = gpx.fit_scipy(
-            model= self.posterior,
-            objective= self.object,
-            train_data= self.dataset,
+            model=self.posterior,
+            objective=self.object,
+            train_data=self.dataset,
         )
 
+    def predict(self, x):
+        """
+        Makes predictions on new data.
 
-    def predict(self,x):
+        The method computes the predictive distribution of the optimized model for new input features.
+        It returns the predictive distribution along with the mean and standard deviation of the predictions.
+
+        Args:
+            x: The feature matrix for which predictions are to be made.
+
+        Returns:
+            A tuple containing the predictive distribution, predictive mean, and predictive standard deviation.
+        """        
         latent_dist = self.opt_posterior.predict(x, train_data=self.dataset)
         predictive_dist = self.opt_posterior.likelihood(latent_dist)
 
         predictive_mean = predictive_dist.mean()
         predictive_std = predictive_dist.stddev()
-        return predictive_mean, predictive_std
+        return predictive_dist, predictive_mean, predictive_std
