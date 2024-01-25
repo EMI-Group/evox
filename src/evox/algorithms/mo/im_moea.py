NSGA-II fix the calculation of crowding distance

src/evox/algorithms/mo/im_moea.py
@@ -22,6 +22,7 @@ import optax as ox
 import sys
 import os
 
+
 class IMMOEA(Algorithm):
     def __init__(
         self,
@@ -30,7 +31,7 @@ class IMMOEA(Algorithm):
         n_objs=3,
         pop_size=100,
         l=3,
-        k = 10,
+        k=10,
         selection_op=None,
         mutation_op=None,
     ):
@@ -76,7 +77,7 @@ class IMMOEA(Algorithm):
     def ask(self, state):
         # Block the output of gpjax
         old_stdout = sys.stdout
-        new_stdout = open(os.devnull, 'w')
+        new_stdout = open(os.devnull, "w")
         sys.stdout = new_stdout
         population = state.population
         fitness = state.fitness
@@ -95,7 +96,9 @@ class IMMOEA(Algorithm):
         OffspringDec = jnp.vstack(sub_pops)
         sys.stdout = old_stdout
         new_stdout.close()
-        return OffspringDec, state.update(next_generation=OffspringDec, partition=partition)
+        return OffspringDec, state.update(
+            next_generation=OffspringDec, partition=partition
+        )
 
     # select next generation
     def tell(self, state, fitness):
@@ -119,7 +122,6 @@ class IMMOEA(Algorithm):
         state = state.update(population=survivor, fitness=survivor_fitness)
         return state
 
-
     def _gen_offspring(self, state, sub_pop, sub_fit):
         key, sel_key, x_key, mut_key = jax.random.split(state.key, 4)
         N, D = sub_pop.shape
@@ -130,7 +132,9 @@ class IMMOEA(Algorithm):
             shuffled_indices = random.permutation(x_key, indices)
             while len(shuffled_indices) < L * self.n_objs:
                 _key, x_key = random.split(x_key)
-                shuffled_indices = jnp.vstack((shuffled_indices, random.permutation(x_key, indices)))
+                shuffled_indices = jnp.vstack(
+                    (shuffled_indices, random.permutation(x_key, indices))
+                )
             start = 0
             fmin = 1.5 * jnp.min(sub_fit, axis=0) - 0.5 * jnp.max(sub_fit, axis=0)
             fmax = 1.5 * jnp.max(sub_fit, axis=0) - 0.5 * jnp.min(sub_fit, axis=0)
@@ -140,26 +144,34 @@ class IMMOEA(Algorithm):
             # Train one groups of GP models for each objective
             for m in range(self.n_objs):
                 permutation = random.permutation(sel_keys[m], N)
-                parents = permutation[:jnp.floor(N / self.n_objs).astype(jnp.int32)]
-                sub_off = sub_pop[parents,:]
-                indices = shuffled_indices[start:start+L]
-                keys = x_keys[start:start+L]
+                parents = permutation[: jnp.floor(N / self.n_objs).astype(jnp.int32)]
+                sub_off = sub_pop[parents, :]
+                indices = shuffled_indices[start : start + L]
+                keys = x_keys[start : start + L]
                 start += L
                 likelihood = Gaussian(num_datapoints=len(parents))
                 model = GPRegression(likelihood=likelihood, kernel=Linear())
                 for d, key in zip(indices, keys):
-                    model.fit_scipy(x=sub_fit[parents,m:m+1], y=sub_off[:,d:d+1])
-                    inputs = jnp.linspace(fmin[m], fmax[m], sub_off.shape[0])[:,jnp.newaxis]
+                    model.fit_scipy(
+                        x=sub_fit[parents, m : m + 1], y=sub_off[:, d : d + 1]
+                    )
+                    inputs = jnp.linspace(fmin[m], fmax[m], sub_off.shape[0])[
+                        :, jnp.newaxis
+                    ]
                     ymu, ystd = model.predict(inputs)
                     # get next generation
-                    sub_off = sub_off.at[:,d].set(ymu + random.normal(key, shape=ystd.shape) * ystd)
+                    sub_off = sub_off.at[:, d].set(
+                        ymu + random.normal(key, shape=ystd.shape) * ystd
+                    )
                 if Offspring is None:
                     Offspring = sub_off
                 else:
                     Offspring = jnp.vstack((Offspring, sub_off))
 
         # Convert invalid values to random values
-        randDec = jax.random.uniform(x_key, Offspring.shape, minval=self.lb, maxval=self.ub)
+        randDec = jax.random.uniform(
+            x_key, Offspring.shape, minval=self.lb, maxval=self.ub
+        )
         invalid = (Offspring < self.lb) | (Offspring > self.ub)
         Offspring = jnp.where(invalid, randDec, Offspring)
 

src/evox/algorithms/mo/rm_meda.py
@@ -15,22 +15,9 @@ from evox.operators import (
 import jax.lax as lax
 from functools import partial
 
-# @partial(jax.jit, static_argnums=[0, 1, 2])
-def local_pca(pop_dec, M, K, key):
-    n, d = pop_dec.shape  # Dimensions
-    # d = 12
 
-    # model = [
-    #     {
-    #         "mean": pop_dec[k],  # The mean of the model
-    #         "PI": jnp.eye(d),  # The matrix PI
-    #         "e_vector": [],  # The eigenvectors
-    #         "e_value": [],  # The eigenvalues
-    #         "a": [],  # The lower bound of the projections
-    #         "b": [],
-    #     }
-    #     for k in range(K)
-    # ]  # The upper bound of the projections
+def local_pca(pop_dec, M, K, key):
+    n, d = pop_dec.shape  # n: number of solutions, d: number of decision variables
 
     def create_model(k):
         return {
@@ -41,6 +28,7 @@ def local_pca(pop_dec, M, K, key):
             "a": jnp.array([]),  # The lower bound of the projections
             "b": jnp.array([]),  # The upper bound of the projections
         }
+
     arr = jnp.arange(K)
     model = jax.vmap(create_model)(arr)
 
@@ -50,30 +38,27 @@ def local_pca(pop_dec, M, K, key):
         def distance_fun(k):
             diff = pop_dec - jnp.tile(model["mean"][k], (n, 1))
             return jnp.sum(diff.dot(model["PI"][k]) * diff, axis=1)
+
         distance = jax.vmap(distance_fun, out_axes=1)(arr)
         partition = jnp.argmin(distance, axis=1)
         # Update the model of each cluster
         updated = jnp.zeros(K, dtype=bool)
         for k in range(K):
-        # def body_fun2(k):
+            # def body_fun2(k):
             old_mean = model["mean"][k]
             # select current cluster
             current = partition == k
             if jnp.sum(current) < 2:
                 if not jnp.any(current):
                     current = random.randint(shape=(1,), key=key, maxval=n, minval=0)
-                # def false_fun2(current):
-                #     random_index = random.randint(shape=(1,), key=key, maxval=n, minval=0)
-                #     bool_matrix = jnp.equal(jnp.arange(current.size), random_index)
-                #     # jnp.array([random.randint(shape=(1,), key=key, maxval=n, minval=0)], dtype=int)
-                #     return bool_matrix
-                # current = lax.cond(jnp.any(current), lambda x: x, false_fun2, current)
-                model["mean"] = model['mean'].at[k].set(pop_dec[current].reshape(-1))
+                model["mean"] = model["mean"].at[k].set(pop_dec[current].reshape(-1))
                 model["PI"] = model["PI"].at[k].set(jnp.eye(d))
                 model["e_vector"] = model["e_vector"].at[k].set([])
                 model["e_value"] = model["e_value"].at[k].set([])
             else:
-                model["mean"] = model["mean"].at[k].set(jnp.mean(pop_dec[current], axis=0))
+                model["mean"] = (
+                    model["mean"].at[k].set(jnp.mean(pop_dec[current], axis=0))
+                )
                 cc = jnp.cov(
                     (
                         pop_dec[current]
@@ -81,17 +66,21 @@ def local_pca(pop_dec, M, K, key):
                     ).T
                 )
                 # Using eigh for Hermitian matrices
-                e_value, e_vector = jnp.linalg.eigh(
-                    cc
-                )
+                e_value, e_vector = jnp.linalg.eigh(cc)
                 rank = jnp.argsort(e_value)
                 e_value = jnp.sort(e_value)
                 model["e_value"] = model["e_value"].at[k].set(e_value)
                 model["e_vector"] = model["e_vector"].at[k].set(e_vector[:, rank])
                 # Note: this code using maximum eigenvalues instead of minimum eigenvalues in PlateEmo
-                model["PI"] = model["PI"].at[k].set(model["e_vector"][k:k+1, (M - 1):].dot(
-                    model["e_vector"][k:k+1, (M - 1):].conj().transpose()
-                ))
+                model["PI"] = (
+                    model["PI"]
+                    .at[k]
+                    .set(
+                        model["e_vector"][k : k + 1, (M - 1) :].dot(
+                            model["e_vector"][k : k + 1, (M - 1) :].conj().transpose()
+                        )
+                    )
+                )
             updated = updated.at[k].set(
                 (not jnp.any(current))
                 or (jnp.sqrt(jnp.sum((old_mean - model["mean"][k]) ** 2)) > 1e-5)
@@ -101,19 +90,22 @@ def local_pca(pop_dec, M, K, key):
             break
 
     # Calculate the smallest hyper-rectangle of each model
-    # for k in range(K):
     def body_fun(k):
         if len(model["e_vector"][k]) != 0:
             hyper_rectangle = (
                 pop_dec[partition == k]
                 - jnp.tile(model["mean"][k], (jnp.sum(partition == k), 1))
             ).dot(model["e_vector"][k][:, : M - 1])
-            model["a"] = model['a'].at[k].set(jnp.min(hyper_rectangle, axis=0))
-            model["b"] = model['b'].at[k].set(jnp.max(hyper_rectangle, axis=0))  # this should by tested
+            model["a"] = model["a"].at[k].set(jnp.min(hyper_rectangle, axis=0))
+            model["b"] = (
+                model["b"].at[k].set(jnp.max(hyper_rectangle, axis=0))
+            )  # this should by tested
         else:
             model["a"] = model["a"].at[k].set(jnp.zeros(M - 1))
             model["b"] = model["b"].at[k].set(jnp.zeros(M - 1))
+
     jax.vmap(body_fun)(arr)
+
     ## Calculate the probability of each cluster for reproduction
     # Calculate the volume of each cluster
     volume = jnp.asarray([model["b"][k] for k in range(K)]) - jnp.asarray(