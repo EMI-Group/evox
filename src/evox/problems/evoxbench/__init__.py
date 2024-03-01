09317ba674a682ccd6594a6c8a9bc1978f73b4a0
Bill Huang <bill.huang2001@gmail.com>
2024-03-01 14:35:54 +0800

    dev: handle evoxbench not installed error

--- src/evox/problems/evoxbench/__init__.py
+++ src/evox/problems/evoxbench/__init__.py
@@ -1,1 +1,12 @@
-from .evoxbench import C10MOP, CitySegMOP, IN1kMOP
+try:
+    from .evoxbench import C10MOP, CitySegMOP, IN1kMOP
+except ImportError:
+
+    def C10MOP(*args, **kwargs):
+        raise ImportError("evoxbench is not installed")
+
+    def CitySegMOP(*args, **kwargs):
+        raise ImportError("evoxbench is not installed")
+
+    def IN1kMOP(*args, **kwargs):
+        raise ImportError("evoxbench is not installed")
