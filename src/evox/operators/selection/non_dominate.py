af6aff941c9472082105e8dd1935ba79fdec094c
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-07-01 12:06:18 +0800

    fix missing numpy package in non_dominate

--- src/evox/operators/selection/non_dominate.py
+++ src/evox/operators/selection/non_dominate.py
@@ -3,6 +3,7 @@ from functools import partial
 import jax
 import jax.numpy as jnp
 from jax import jit, lax, pure_callback, vmap
+import numpy as np
 
 from evox import jit_class
 from evox.utils import dominate_relation
