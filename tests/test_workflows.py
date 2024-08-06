f1ce079603114f78e101cd1bc990a317fcee77f4
Bill Huang <bill.huang2001@gmail.com>
2024-08-06 17:00:31 +0800

    test: disable nonjitworkflow test

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -90,31 +90,6 @@ def test_std_workflow():
     assert min_fitness1 < 1e-4
 
 
-def test_non_jit_workflow():
-    monitor = StdSOMonitor()
-    # create a workflow
-    workflow = workflows.NonJitWorkflow(
-        algorithm=algorithms.CSO(
-            lb=jnp.full(shape=(2,), fill_value=-32),
-            ub=jnp.full(shape=(2,), fill_value=32),
-            pop_size=20,
-        ),
-        problem=problems.numerical.Ackley(),
-        monitors=[monitor],
-    )
-    # init the workflow
-    key = jax.random.PRNGKey(42)
-    state = workflow.init(key)
-
-    # run the workflow for 100 steps
-    for i in range(100):
-        state = workflow.step(state)
-
-    # the result should be close to 0
-    min_fitness = monitor.get_best_fitness()
-    assert min_fitness < 1e-4
-
-
 def test_distributed_cso():
     monitor = StdSOMonitor()
     # create a workflow
