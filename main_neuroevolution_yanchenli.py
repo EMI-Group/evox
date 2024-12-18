5fb8fc86edd82fef6c8314c50d809307a467f3dd
liyc5929 <48726053+liyc5929@users.noreply.github.com>
2024-12-18 16:46:14 +0800

    Add main neuroevolution testing file.

--- main_neuroevolution_yanchenli.py
+++ main_neuroevolution_yanchenli.py
@@ -1,13 +1,14 @@
 import time
+import os
 import torch
 from torch.profiler import profile, ProfilerActivity
+
 from src.core import Problem, use_state, jit
 from src.algorithms import PSO
 from src.workflows import StdWorkflow
 
 
 if __name__ == "__main__":
-
     class Sphere(Problem):
         def __init__(self):
             super().__init__()
@@ -24,15 +25,26 @@ if __name__ == "__main__":
     workflow.setup(algo, prob)
     workflow.step()
     workflow.__sync__()
-    with open("tests/a.md", "w") as ff:
+
+    log_root = "./tests"
+    os.makedirs(log_root, exist_ok=True)
+
+    log_file_a = os.path.join(log_root, "a.md")
+    with open(log_file_a, "w") as ff:
         ff.write(workflow.step.inlined_graph.__str__())
+    print(f"Please see the result log at `{log_file_a}`.")
+
     state_step = use_state(lambda: workflow.step)
     state = state_step.init_state()
     ## state = {k: (v if v.ndim < 1 or v.shape[0] != algo.pop_size else v[:3]) for k, v in state.items()}
     jit_state_step = jit(state_step, trace=True, example_inputs=(state,))
     state = state_step.init_state()
-    with open("tests/b.md", "w") as ff:
+
+    log_file_b = os.path.join(log_root, "b.md")
+    with open(log_file_b, "w") as ff:
         ff.write(jit_state_step.inlined_graph.__str__())
+    print(f"Please see the result log at `{log_file_b}`.")
+
     t = time.time()
     with profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True
