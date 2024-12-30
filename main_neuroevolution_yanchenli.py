3c245744f650695bf8b23676ac01ff6239432459
liyc5929 <48726053+liyc5929@users.noreply.github.com>
2024-12-30 16:29:14 +0800

    Add raw executable neuroevolution module support and test.

--- main_neuroevolution_yanchenli.py
+++ main_neuroevolution_yanchenli.py
@@ -9,7 +9,7 @@ from torch.profiler import profile, ProfilerActivity
 from src.utils import ParamsAndVector
 from src.core import Problem, use_state, jit
 from src.algorithms import PSO
-from src.workflows import StdWorkflow
+from src.workflows import StdWorkflow, EvalMonitor
 from src.problems.neuroevolution import SupervisedLearningProblem
 
 class Print(nn.Module):
@@ -23,7 +23,7 @@ class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
         self.features = nn.Sequential(
-            Print(),
+            # Print(),
             nn.Conv2d(1, 3, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
@@ -46,8 +46,9 @@ class SimpleCNN(nn.Module):
 
 if __name__ == "__main__":
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    # torch.set_default_device(device)
     data_root = "./data"
     os.makedirs(data_root, exist_ok=True)
 
@@ -58,36 +59,80 @@ if __name__ == "__main__":
         transform = torchvision.transforms.ToTensor(),
     )
     train_loader = DataLoader(train_dataset,
-        batch_size = 19,
+        batch_size = 10,
         shuffle    = True,
         collate_fn = None,
     )
+    test_dataset = torchvision.datasets.MNIST(
+        root      = data_root,
+        train     = False,
+        download  = True,
+        transform = torchvision.transforms.ToTensor(),
+    )
+    test_loader = DataLoader(test_dataset,
+        batch_size = 10,
+        shuffle    = False,
+        collate_fn = None,
+    )
 
-    model = SimpleCNN()
+    import tqdm
+    trainloader = [
+        (inputs.to(device), labels.to(device))
+        for inputs, labels in tqdm.tqdm(train_loader)
+    ]
+    testloader = [
+        (inputs.to(device), labels.to(device))
+        for inputs, labels in tqdm.tqdm(test_loader)
+    ]
+
+
+    model = SimpleCNN().to(device=device)
     total_params = sum(p.numel() for p in model.parameters())
     print(f"Total number of parameters: {total_params}")
 
-    adapter = ParamsAndVector(dummy_model=model)
+    adapter = ParamsAndVector(dummy_model=model).to(device=device)
     model_params = dict(model.named_parameters())
     
-    prob = SupervisedLearningProblem(
-        criterion   = nn.CrossEntropyLoss(),
-        data_loader=train_loader,
-    )
-    prob.setup(model=model, adapter=adapter)
+    prob = SupervisedLearningProblem(data_loader=train_loader)
+    prob.setup(model=model, criterion=nn.MSELoss(), device=device)
     prob.to(device)
 
     center = adapter.to_vector(model_params)
 
-    algo = PSO(pop_size=11).to(device)
+    algo = PSO(pop_size=23).to(device)
     algo.setup(lb=center - 10, ub=center + 10)
+    monitor = EvalMonitor(topk=1) # best one
+    monitor.setup()
+    monitor.to(device)
     workflow = StdWorkflow()
     workflow.setup(
         algorithm          = algo, 
         problem            = prob,
         solution_transform = adapter,
+        monitor            = monitor,
+        device             = device
     )
-    workflow.step()
+    for _ in range(50):
+        workflow.step()
+
+        monitor = workflow.get_submodule("monitor")
+        print(monitor.topk_fitness[0])
+        best_params = adapter.to_params(monitor.topk_solutions[0])
+        model.load_state_dict(best_params)
+        model.eval()
+        total = 0
+        correct = 0
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                inputs = inputs.to(device=device, non_blocking=True)
+                labels = labels.to(device=device, non_blocking=True)
+    
+                logits = model(inputs)
+                _, predicted = torch.max(logits.data, dim=1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print(f"Acc: {100 * correct / total} %.")
+
 
     # -----------------------------------------
     import sys; sys.exit(1)
--- src/problems/neuroevolution/supervised_learning.py
+++ src/problems/neuroevolution/supervised_learning.py
@@ -3,9 +3,8 @@ import types
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from typing import Callable, Dict, Tuple
+from typing import Dict, Tuple
 
-from ...utils import ParamsAndVector
 from ...core import Problem, jit_class, use_state, vmap, jit
 from ...core.module import assign_load_state_dict
 
@@ -16,76 +15,112 @@ __data_loader__: Dict[int, DataLoader] = None # TODO: Add to __init__
 @jit_class
 class SupervisedLearningProblem(Problem):
 
-    def __init__(self,
-        data_loader: DataLoader,
-        criterion  : Callable,
-    ):
+    def __init__(self, data_loader: DataLoader):
         super().__init__()
-        # self.data_loader = data_loader
         global __data_loader__
         __data_loader__ = data_loader
-        self.criterion   = criterion
     
-    def setup(self, model: nn.Module, adapter: ParamsAndVector):
-        inference_model = copy.deepcopy(model)
+    def setup(self, model: nn.Module, criterion: nn.Module, device: torch.device | None = None):
+        device = torch.get_default_device() if device is None else device
+        inference_model = copy.deepcopy(model) # TODO
+        inference_model = inference_model.to(device=device)
         for _, value in inference_model.named_parameters():
             value.requires_grad = False
         inference_model.load_state_dict = types.MethodType(assign_load_state_dict, inference_model)
 
         state_forward       = use_state(lambda: inference_model.forward)
-        param_keys    = tuple(dict(inference_model.named_parameters()).keys())
+        param_keys          = tuple(dict(inference_model.named_parameters()).keys())
         self._model_buffers = {
             key: value 
             for key, value in state_forward.init_state().items() if key not in param_keys
         }
+        # FIXME
         self._example_param_ndim = (param_keys[0], inference_model.get_parameter(param_keys[0]).ndim)
 
         global __data_loader__
-        vmap_state_forward           = vmap(state_forward, in_dims=(0, None))
-        init_state = vmap_state_forward.init_state(11)
-        self._jit_vmap_state_forward = jit(vmap_state_forward, 
-            trace=True, lazy=False, 
-            example_inputs=(init_state, next(iter(__data_loader__))[0])
-        ) 
-        self._jit_forward   = torch.jit.script(inference_model)
-        self._jit_criterion = torch.jit.script(self.criterion)
+        dummy_inputs, dummy_labels = next(iter(__data_loader__))
+        dummy_inputs = dummy_inputs.to(device=device)
+        dummy_labels = dummy_labels.to(device=device)
 
-        self.model       = inference_model
+        vmap_state_forward = vmap(state_forward, in_dims=(0, None))
+        model_init_state   = vmap_state_forward.init_state(23) # TODO: move to user side
+        self._jit_vmap_state_forward, (_, dummy_logits) = jit(vmap_state_forward, 
+            trace=True, lazy=False, return_dummy_output=True, 
+            example_inputs=(model_init_state, dummy_inputs)
+        )
+        self._jit_forward  = torch.jit.script(inference_model)
+
+        dummy_labels = dummy_labels.unsqueeze(1).repeat(1, 10) # TODO: move to user side
+
+        state_criterion      = use_state(lambda: criterion.forward)
+        if len(state_criterion.init_state()) > 0:
+            vmap_state_criterion = vmap(state_criterion, in_dims=(0, 0, None))
+            criterion_init_state = vmap_state_criterion.init_state(23)
+            self._jit_vmap_state_criterion = jit(vmap_state_criterion, 
+                trace=True, lazy=False, 
+                example_inputs=(criterion_init_state, dummy_logits, dummy_labels),
+            )
+        else:
+            vmap_state_criterion = vmap(state_criterion, in_dims=(0, None))
+            self._jit_vmap_state_criterion = jit(vmap_state_criterion, 
+                trace=True, lazy=False, 
+                example_inputs=(dummy_logits, dummy_labels),
+            )
+            criterion_init_state = None
+
+        self._jit_criterion  = torch.jit.script(criterion)
+
+        self.model                = inference_model
+        self.criterion            = criterion
+        self.criterion_init_state = criterion_init_state
 
     @torch.jit.export
-    def _vmap_state_forward(self, state: Dict[str, torch.Tensor], inputs: torch.Tensor) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
-        print(state[self._example_param_ndim[0]])
+    def _vmap_state_forward(self, 
+            state : Dict[str, torch.Tensor], 
+            inputs: torch.Tensor,
+        ) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
         return self._jit_vmap_state_forward(state, inputs)
 
     @torch.jit.export
-    def _criterion(self, logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
-        return self._jit_criterion(logits, labels)
+    def _vmap_state_criterion(self, 
+            state : Dict[str, torch.Tensor] | None, 
+            logits: torch.Tensor, 
+            labels: torch.Tensor,
+        ) -> torch.Tensor:
+        # TODO: check empty state and branch
+        return self._jit_vmap_state_criterion(logits, labels)
 
     @torch.jit.ignore
-    def _evaluate_vmap(self, pop_params: Dict[str, nn.Parameter], device: torch.device, num_map: int):
+    def _evaluate_vmap(self, pop_params: Dict[str, nn.Parameter], num_map: int, device: torch.device):
         model_buffers = {
             key: value.unsqueeze(0).expand(num_map, *([-1] * value.ndim)) 
             for key, value in self._model_buffers.items()
         }
         # state = {**model_buffers, **pop_params}
-        state = model_buffers
-        state.update(pop_params)
+        model_state = model_buffers
+        pop_params = {"self." + key: val for key, val in pop_params.items()}
+        model_state.update(pop_params)
+        criterion_state = copy.deepcopy(self.criterion_init_state)
+        
         n_inputs = 0
-        result = torch.tensor(0, device=device)
+        result = torch.zeros(num_map, device=device)
         global __data_loader__
-        for (inputs, labels) in __data_loader__:
-            inputs.to(device=device, non_blocking=True)
-            labels.to(device=device, non_blocking=True)
+        for v in __data_loader__:
+            inputs: torch.Tensor = v[0]
+            labels: torch.Tensor = v[1]
+            inputs = inputs.to(device=device, non_blocking=True)
+            labels = labels.to(device=device, non_blocking=True)
+            labels = labels.unsqueeze(1).repeat(1, 10) # TODO: move to user side
 
-            state, logits = self._vmap_state_forward(state, inputs)
+            model_state, logits = self._vmap_state_forward(model_state, inputs)
             n_inputs     += inputs.size(0)
-            result       += self._criterion(logits, labels) * inputs.size(0)
+            result       += self._vmap_state_criterion(criterion_state, logits, labels) * inputs.size(0)
 
         pop_fitness = result / n_inputs
         return pop_fitness
 
     @torch.jit.ignore
-    def _evaluate_single(self, params: Dict[str, nn.Parameter], device: torch.device):
+    def _evaluate_single(self, params: Dict[str, nn.Parameter], device: torch.device): # FIXME
         self.model.load_state_dict(params)
 
         result = torch.tensor(0, device=device)
@@ -105,10 +140,10 @@ class SupervisedLearningProblem(Problem):
     def evaluate(self, pop_params: Dict[str, nn.Parameter]) -> torch.Tensor:
         pop_params_value = pop_params[self._example_param_ndim[0]]
         if pop_params_value.ndim > self._example_param_ndim[1] and pop_params_value.size(0) != 1: 
-        # vmapped evaluation
-            pop_fitness = self._evaluate_vmap(pop_params, pop_params_value.device, pop_params_value.size(0))
+            # vmapped evaluation
+            pop_fitness = self._evaluate_vmap(pop_params, pop_params_value.size(0), pop_params_value.device)
         else: 
-        # single evaluation
+            # single evaluation
             pop_fitness = self._evaluate_single(pop_params, pop_params_value.device)
         return pop_fitness 
 
