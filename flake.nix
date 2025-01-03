ca004c9bb09f1ce648bc825fd2f0ea905f919f57
Bill Huang <bill.huang2001@gmail.com>
2025-01-03 15:26:10 +0800

    Update nix's develop environment

--- flake.nix
+++ flake.nix
@@ -1,30 +1,25 @@
 {
   description = "evox";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, utils, flake-compat }:
+  outputs = { self, nixpkgs, utils }:
     with utils.lib;
     with nixpkgs.lib;
     eachSystem (with system; [ x86_64-linux ]) (system:
       let
         builder = import ./dev_env_builder.nix;
         cuda-env = builder { inherit system nixpkgs; cudaSupport = true; };
+        rocm-env = builder { inherit system nixpkgs; rocmSupport = true; };
         cpu-env = builder { inherit system nixpkgs; cudaSupport = false; };
       in
       {
         devShells.default = cpu-env;
         devShells.cpu = cpu-env;
         devShells.cuda = cuda-env;
-        devShells.fhs = import ./fhs.nix {
-          inherit nixpkgs system; cudaSupport=true;
-        };
+        devShells.rocm = rocm-env;
       }
     );
 }
