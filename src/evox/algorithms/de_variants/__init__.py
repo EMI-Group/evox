79dea2cdc296bfa5c03481d9849975ce573018ee
Kevin Sun <sunkebin.cn@gmail.com>
2025-01-13 14:26:39 +0800

    Update __init__.py for de_variants

--- src/evox/algorithms/de_variants/__init__.py
+++ src/evox/algorithms/de_variants/__init__.py
@@ -1,9 +1,9 @@
-__all__ = ["DE", "SHADE", "CoDE", "JaDE", "ODE"]
+__all__ = ["DE", "CoDE", "JaDE", "ODE", "SaDE", "SHADE"]
 
 
 from .de import DE
-from .shade import SHADE
 from .code import CoDE
-from .sade import SaDE
 from .jade import JaDE
 from .ode import ODE
+from .sade import SaDE
+from .shade import SHADE
