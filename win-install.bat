40e0bd3767d85dbb5f1a99afdb8c5274612b6d89
Bill Huang <bill.huang2001@gmail.com>
2025-01-14 22:12:02 +0800

    Optimize windows install script

--- win-install.bat
+++ win-install.bat
@@ -87,7 +87,7 @@ pip install "evox>=1.0.0a1"
 REM Download some demo
 mkdir %UserProfile%\evox-demo
 @REM curl -L -o %UserProfile%\evox-demo\custom_algo_prob.ipynb https://raw.githubusercontent.com/EMI-Group/evox/refs/heads/evoxtorch-main/docs/source/example/custom_algo_prob.ipynb
-git clone -b evoxtorch-main https://github.com/EMI-Group/evox.git %UserProfile%\evox-demo\evox
+git clone --depth 1 https://github.com/EMI-Group/evox.git %UserProfile%\evox-demo\evox
 xcopy %UserProfile%\evox-demo\evox\docs\source\example\custom_algo_prob.ipynb %UserProfile%\evox-demo /E /I /Y
 start code %UserProfile%\evox-demo
 
