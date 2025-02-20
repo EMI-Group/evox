e0cd37c333e097f5a3c6d71b995cfecc699315c9
Bill Huang <bill.huang2001@gmail.com>
2025-02-20 17:48:58 +0800

    Fix test_std_workflow

--- unit_test/workflows/test_std_workflow.py
+++ unit_test/workflows/test_std_workflow.py
@@ -3,24 +3,19 @@ import unittest
 import torch
 import torch.nn as nn
 
-from evox.core import Algorithm, Mutable, Problem, jit, trace_impl, use_state, vmap
+from evox.core import Algorithm, Mutable, Problem, use_state, vmap
 from evox.workflows import EvalMonitor, StdWorkflow
 
 
 class BasicProblem(Problem):
     def __init__(self):
         super().__init__()
-        self._eval_fn = vmap(BasicProblem._single_eval, trace=False)
-        self._eval_fn_traced = vmap(BasicProblem._single_eval, example_ndim=2)
+        self._eval_fn = vmap(BasicProblem._single_eval)
 
     def _single_eval(x: torch.Tensor, p: float = 2.0):
         return (x**p).sum()
 
     def evaluate(self, pop: torch.Tensor):
-        return self._eval_fn_traced(pop)
-
-    @trace_impl(evaluate)
-    def trace_evaluate(self, pop: torch.Tensor):
         return self._eval_fn(pop)
 
 
@@ -42,13 +37,6 @@ class BasicAlgorithm(Algorithm):
         self.pop.copy_(pop)
         self.fit.copy_(self.evaluate(pop))
 
-    @trace_impl(step)
-    def trace_step(self):
-        pop = torch.rand(self.pop_size, self.dim, dtype=self.lb.dtype, device=self.lb.device)
-        pop = pop * (self.ub - self.lb)[None, :] + self.lb[None, :]
-        self.pop = pop
-        self.fit = self.evaluate(pop)
-
 
 class TestStdWorkflow(unittest.TestCase):
     def setUp(self):
@@ -59,18 +47,17 @@ class TestStdWorkflow(unittest.TestCase):
         self.workflow.setup(self.algo, self.prob)
 
     def test_basic_workflow(self):
-        state_step = use_state(lambda: self.workflow.step)
-        self.assertIsNotNone(state_step.init_state())
-        jit_step = jit(state_step, trace=True, example_inputs=(state_step.init_state(),))
-        self.assertIsNotNone(jit_step(state_step.init_state()))
+        compiled_step = torch.compile(self.workflow.step)
+        for _ in range(3):
+            compiled_step()
 
     def test_vmap_workflow(self):
-        state_step = use_state(lambda: self.workflow.step)
-        vmap_state_step = vmap(state_step)
-        state = vmap_state_step.init_state(3)
+        state_step = use_state(self.workflow.step)
+        vmap_state_step = vmap(state_step, randomness="different")
+        state = torch.func.stack_module_state([self.workflow] * 3)
         self.assertIsNotNone(state)
-        jit_state_step = jit(vmap_state_step, trace=True, lazy=True)
-        self.assertIsNotNone(jit_state_step(state))
+        compiled_state_step = torch.compile(vmap_state_step)
+        self.assertIsNotNone(compiled_state_step(state))
 
     def test_classic_workflow(self):
         class solution_transform(nn.Module):
