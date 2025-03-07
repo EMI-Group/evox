6841f87c5fddfff977210ef20df7986426ff77d4
Kevin Sun <skk7757@163.com>
2025-03-07 16:16:34 +0800

    Fix use_state output state keys

--- src/evox/core/module.py
+++ src/evox/core/module.py
@@ -170,6 +170,7 @@ def use_state(stateful_func: Union[Callable, nn.Module]) -> Callable:
     def wrapper(params_and_buffers: Dict[str, torch.Tensor], *args, **kwargs):
         params_and_buffers = {"_inner_module." + k: v for k, v in params_and_buffers.items()}
         output = torch.func.functional_call(module, params_and_buffers, *args, **kwargs)
+        params_and_buffers = {k[len("_inner_module."):]: v for k, v in params_and_buffers.items()}
         if output is None:
             return params_and_buffers
         else:
