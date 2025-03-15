1a4441e9b7a246ed9045cd558eed9feda61f8d7a
Bill Huang <bill.huang2001@gmail.com>
2025-03-15 21:41:09 +0800

    When recording batchedtensors, unwrap it to avoid leaking batchedtensors

--- src/evox/workflows/eval_monitor.py
+++ src/evox/workflows/eval_monitor.py
@@ -2,6 +2,7 @@ import warnings
 from typing import Dict, List, Tuple
 
 import torch
+from torch._C._functorch import get_unwrapped, is_batchedtensor
 
 from evox.core import Monitor, Mutable
 from evox.operators.selection import non_dominate_rank
@@ -122,9 +123,15 @@ class EvalMonitor(Monitor):
     def record_history(self):
         if self.full_fit_history or self.full_sol_history:
             if self.full_sol_history:
-                self.solution_history.append(self.latest_solution.to(self.device))
+                latest_solution = self.latest_solution.to(self.device)
+                if is_batchedtensor(self.latest_solution):
+                    latest_solution = get_unwrapped(latest_solution)
+                self.solution_history.append(latest_solution)
             if self.full_fit_history:
-                self.fitness_history.append(self.latest_fitness.to(self.device))
+                latest_fitness = self.latest_fitness.to(self.device)
+                if is_batchedtensor(self.latest_fitness):
+                    latest_fitness = get_unwrapped(latest_fitness)
+                self.fitness_history.append(latest_fitness)
 
     def get_latest_fitness(self) -> torch.Tensor:
         """Get the fitness values from the latest iteration."""
