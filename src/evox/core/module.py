555310857bc70f9d5a3227ed14acdfaf6db2ae9a
XU-Boqing <xuboqing123@gmail.com>
2025-04-03 12:49:24 +0800

    Update module.py

--- src/evox/core/module.py
+++ src/evox/core/module.py
@@ -158,7 +158,7 @@ def use_state(stateful_func: Union[Callable, nn.Module]) -> Callable:
     """
     if not isinstance(stateful_func, torch.nn.Module):
         module: torch.nn.Module = stateful_func.__self__
-        assert isinstance(module, (torch.nn.Module)), (
+        assert isinstance(module, torch.nn.Module), (
             "`stateful_func` must be a `torch.nn.Module` or a method of a `torch.nn.Module`"
         )
         new_forward = stateful_func.__func__
