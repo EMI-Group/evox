e98ec1ed1759edaab6af27cf9e1809157c9d07f4
Nam-dada <12011815@mail.sustech.edu.cn>
2025-04-10 16:50:09 +0800

    Add vmap test

--- unit_test/problems/test_mujoco.py
+++ unit_test/problems/test_mujoco.py
@@ -4,7 +4,8 @@ import unittest
 import torch
 import torch.nn as nn
 
-from evox.algorithms import PSO
+from evox.algorithms import DE, PSO
+from evox.problems.hpo_wrapper import HPOFitnessMonitor, HPOProblemWrapper
 from evox.problems.neuroevolution.mujoco_playground import MujocoProblem
 from evox.utils import ParamsAndVector
 from evox.workflows import EvalMonitor, StdWorkflow
@@ -152,3 +153,75 @@ class TestMujocoProblem(unittest.TestCase):
         print(f"\tBest params: {best_params}")
 
         problem.visualize(best_params)
+
+    def test_hpo_mujoco_problem(self):
+        model = SimpleMLP().to(self.device)
+        for p in model.parameters():
+            p.requires_grad = False
+        total_params = sum(p.numel() for p in model.parameters())
+        print(f"Total number of model parameters: {total_params}")
+        test_model(model, self.device)
+        print()
+
+        adapter = ParamsAndVector(dummy_model=model)
+        model_params = dict(model.named_parameters())
+        pop_center = adapter.to_vector(model_params)
+        lower_bound = torch.full_like(pop_center, -5)
+        upper_bound = torch.full_like(pop_center, 5)
+
+        POP_SIZE = 17
+        OUTER_POP = 7
+        problem = MujocoProblem(
+            policy=model,
+            env_name="SwimmerSwimmer6",
+            max_episode_length=1000,
+            num_episodes=3,
+            pop_size=POP_SIZE * OUTER_POP,
+            device=self.device,
+        )
+
+        algorithm = PSO(
+            pop_size=POP_SIZE,
+            lb=lower_bound,
+            ub=upper_bound,
+            device=self.device,
+        )
+
+        pop_monitor = HPOFitnessMonitor()
+
+        workflow = StdWorkflow(
+            algorithm=algorithm,
+            problem=problem,
+            monitor=pop_monitor,
+            opt_direction="max",
+            solution_transform=adapter,
+            device=self.device,
+        )
+
+        outer_prob = HPOProblemWrapper(
+            10, num_instances=OUTER_POP, workflow=workflow, copy_init_state=False
+        )
+        outer_algo = DE(
+            OUTER_POP,
+            lb=torch.zeros(3, device=self.device),
+            ub=torch.tensor([1.0, 5.0, 2.0], device=self.device),
+            device=self.device,
+        )
+        outer_workflow = StdWorkflow(
+            outer_algo,
+            outer_prob,
+            solution_transform=lambda x: {
+                "algorithm.w": x[:, 0],
+                "algorithm.phi_p": x[:, 1],
+                "algorithm.phi_g": x[:, 2],
+            },
+            device=self.device,
+        )
+        compiled_step = torch.compile(outer_workflow.step)
+        compiled_step()
+
+        for index in range(3):
+            print(f"In generation {index}:")
+            t = time.time()
+            compiled_step()
+        print(f"\tTime elapsed: {time.time() - t: .4f}(s).")
