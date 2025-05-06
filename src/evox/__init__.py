93f80a858075f063e16066b1166478b91a487956
Bill Huang <bill.huang2001@gmail.com>
2025-05-06 14:46:26 +0800

    Re-export commonly used functions to top-level

--- src/evox/__init__.py
+++ src/evox/__init__.py
@@ -1,6 +1,26 @@
-__all__ = ["core", "utils", "algorithms", "problems", "workflows", "operators", "vis_tools", "metrics"]
+__all__ = [
+    # modules
+    "core",
+    "utils",
+    "algorithms",
+    "problems",
+    "workflows",
+    "operators",
+    "vis_tools",
+    "metrics",
+    # re-exported classes and functions
+    "ModuleBase",
+    "Mutable",
+    "Parameter",
+    "compile",
+    "use_state",
+    "vmap",
+]
 
-from . import algorithms, core, metrics, operators, problems, utils, vis_tools, workflows
+from . import core
+# re-export everything from core, so that users can access it directly
+from .core import ModuleBase, Mutable, Parameter, compile, use_state, vmap
+from . import algorithms, metrics, operators, problems, utils, vis_tools, workflows
 
 
 # After that, try loading extensions from `evox_ext` package
